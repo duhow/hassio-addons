1a09406dd2350f605e7ee317daeb71f5dd23a5dc
duhow <contacto@duhowpi.net>
2021-06-12 17:29:48 +0200

    Add sample rate config

--- vosk/root/entrypoint.sh
+++ vosk/root/entrypoint.sh
@@ -7,4 +7,6 @@ bashio::log.info "Preparing to run Vosk"
 LANGUAGE=$(bashio::config "language[0]")
 [ -z "$LANGUAGE" ] && LANGUAGE=en
 
+VOSK_SAMPLE_RATE=$(bashio::config "sample_rate")
+
 python3 /asr_server.py -vv /data/vosk-model/${LANGUAGE}
